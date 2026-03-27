4c15a5db878ce256e72f392de18a2e1ee03c1127
Renato <renato@sunfire.com>
2026-03-27 08:33:21 +0000

    fix(backend): rerun release and image build

--- src/backend/src/backend/main.py
+++ src/backend/src/backend/main.py
@@ -101,6 +101,7 @@ async def read_stats():
 # This comment is used to trigger the CI pipeline when changes are made to this file 2
 # This comment is used to trigger the CI pipeline when changes are made to this file 3
 # This comment is used to trigger the CI pipeline when changes are made to this file 3
+# Marker 4
 
 
 def main():
